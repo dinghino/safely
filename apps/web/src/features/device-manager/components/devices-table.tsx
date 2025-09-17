829fac46913d37588289eb9de91cda7c2d8e08d6
dinghino <dinghino@users.noreply.github.com>
2025-09-17 20:22:09 +0200

    ♻️ web: cleanup devices table

--- apps/web/src/features/device-manager/components/devices-table.tsx
+++ apps/web/src/features/device-manager/components/devices-table.tsx
@@ -1,31 +1,38 @@
 'use client'
 
-import { useMutation, useQuery } from 'convex/react'
 import { api } from '@workspace/backend/convex/_generated/api'
 import type { Doc } from '@workspace/backend/convex/_generated/dataModel'
-
 import {
-  createColumnHelper,
-  DataTableProvider,
   type ColumnDef,
+  createColumnHelper,
   DataTable,
+  DataTableProvider,
   DataTableToolbar,
-  useDataTable,
 } from '@workspace/data-table'
-import dayjs from '@/lib/dayjs'
-import { useMemo } from 'react'
 import { Badge } from '@workspace/ui/components/badge'
-import { useIsCurrent } from '../hooks/use-is-current'
-import { useIsActive } from '../hooks/use-is-active'
-import { cn } from '@/lib/utils'
+import { Button } from '@workspace/ui/components/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@workspace/ui/components/dropdown-menu'
+import { useMutation, useQuery } from 'convex/react'
 import { MoreVertical } from 'lucide-react'
-import { Button } from '@workspace/ui/components/button'
+import { useMemo } from 'react'
+import dayjs from '@/lib/dayjs'
+import { cn } from '@/lib/utils'
+import { useIsActive } from '../hooks/use-is-active'
+import { useIsCurrent } from '../hooks/use-is-current'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@workspace/ui/components/dialog'
+import { Input } from '@workspace/ui/components/input'
 
 type Device = Doc<'devices'> & { id: string }
 
@@ -37,40 +44,44 @@ const useDeviceColumns = () => {
       [
         c.display({
           id: 'active',
-          // header: 'Status',
-          cell: (info) => <ActiveIndicator device={info.row.original} />,
+          header: 'Status',
+          cell: ({ row }) => <ActiveIndicator device={row.original} />,
           enableSorting: false,
         }),
         c.accessor('name', {
-          header: 'Name',
-          cell: (info) => {
-            return (
-              <div className="inline-flex items-center justify-between gap-2">
-                {info.getValue() || <span className="text-muted-foreground">No name</span>}
-                <CurrentIndicator device={info.row.original} />
-              </div>
-            )
-          },
+          id: 'name',
+          header: 'Device',
+          cell: ({ row }) => <DeviceName device={row.original} />,
         }),
         c.accessor('deviceId', {
+          id: 'deviceId',
           header: 'Device ID',
           enableHiding: true,
-          cell: (info) => <Badge>{info.getValue()}</Badge>,
+          maxSize: 120,
+          size: 120,
+          minSize: 120,
+          cell: (info) => (
+            <Badge variant="secondary" className="font-mono">
+              {info.getValue()}
+            </Badge>
+          ),
         }),
         c.accessor('platform', {
+          id: 'platform',
           header: 'Platform',
-          cell: (info) => info.getValue() || <span className="text-muted-foreground">Unknown</span>,
+          cell: ({ row }) => <DevicePlatform device={row.original} />,
         }),
         c.accessor('last_seen', {
+          id: 'last_seen',
           header: 'Last Seen',
-          cell: (info) => dayjs(info.getValue())?.fromNow(),
+          cell: ({ cell }) => dayjs(cell.getValue())?.fromNow(),
         }),
         c.display({
           id: 'actions',
           enableSorting: false,
-          cell: (info) => (
+          cell: ({ row }) => (
             <div className="inline-flex w-full justify-end">
-              <DeviceRowActions device={info.row.original} />
+              <DeviceRowActions device={row.original} />
             </div>
           ),
         }),
@@ -84,9 +95,8 @@ export function DevicesTable() {
   const _unregister = useMutation(api.devices.deleteDevice)
 
   const columns = useDeviceColumns()
-
+  // data-table expects an 'id' field for now
   const data = useMemo(() => devices?.map((d) => ({ ...d, id: d._id })) ?? [], [devices])
-  if (!devices) return null
 
   return (
     <DataTableProvider
@@ -94,36 +104,57 @@ export function DevicesTable() {
       data={data}
       initialState={{ columnVisibility: { deviceId: false } }}
     >
-      <DataTableToolbar>
-        <Toolbar />
-      </DataTableToolbar>
-      <DataTable />
+      <div className="space-y-2">
+        <DataTableToolbar>
+          <Toolbar />
+        </DataTableToolbar>
+        <DataTable />
+      </div>
     </DataTableProvider>
   )
 }
 
 const Toolbar = () => {
-  const { table } = useDataTable()
-
-  return <>{null}</>
+  // const { table } = useDataTable()
+  return <span className="w-full rounded border bg-background px-2 py-1">Toolbar goes here</span>
 }
 
-function CurrentIndicator({ device }: { device: Device }) {
+function DeviceName({ device }: { device: Device }) {
   const isCurrent = useIsCurrent({ device })
-  if (!isCurrent) return null
-  return <Badge variant={isCurrent ? 'default' : 'secondary'}>this device</Badge>
+  const name = device.name ?? 'Unknown device'
+  const hasName = Boolean(device.name)
+  return (
+    <div className="inline-flex w-full items-center justify-between gap-2">
+      {/* <ActiveIndicator device={device} /> */}
+      <span className={cn(!hasName && 'text-muted-foreground', isCurrent && 'font-bold')}>
+        {name}
+      </span>
+      {/* <CurrentIndicator device={row.original} /> */}
+    </div>
+  )
+}
+
+function DevicePlatform({ device }: { device: Device }) {
+  const platform = device.platform ?? 'Unknown platform'
+  const hasPlatform = Boolean(device.platform)
+  return <span className={cn(!hasPlatform && 'text-muted-foreground')}>{platform}</span>
 }
 
 function ActiveIndicator({ device }: { device: Device }) {
   const active = useIsActive({ device })
   return (
-    <Badge className={cn('aspect-square h-4 w-4 p-0', active ? 'bg-green-500' : 'bg-gray-500')} />
+    <Badge
+      variant="secondary"
+      className={cn('max-md:aspect-square', active ? 'text-green-500' : 'text-gray-500')}
+    >
+      <span className={cn('h-2 w-2 rounded-full', active ? 'bg-green-500' : 'bg-gray-500')} />
+      <span className="max-md:sr-only">{active ? 'Online' : 'Offline'}</span>
+    </Badge>
   )
 }
 
 function DeviceRowActions({ device }: { device: Device }) {
   const unregister = useMutation(api.devices.deleteDevice)
-  // const local = useDeviceInfo()
   const isCurrent = useIsCurrent({ device })
 
   return (
@@ -138,7 +169,37 @@ function DeviceRowActions({ device }: { device: Device }) {
         <DropdownMenuItem disabled={isCurrent} onClick={() => unregister({ id: device._id })}>
           Unregister
         </DropdownMenuItem>
+        <Dialog>
+          <DialogTrigger asChild>
+            <DropdownMenuItem onSelect={(e) => e.preventDefault()}>Rename</DropdownMenuItem>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Rename Device</DialogTitle>
+              <DialogDescription>Enter a new name for your device.</DialogDescription>
+            </DialogHeader>
+            <RenameForm device={device} />
+          </DialogContent>
+        </Dialog>
       </DropdownMenuContent>
     </DropdownMenu>
+  )
+}
+
+function RenameForm({ device }: { device: Device }) {
+  const rename = useMutation(api.devices.renameDevice)
+  return (
+    <form
+      className="mt-4 flex w-full gap-2"
+      onSubmit={(e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target as HTMLFormElement)
+        const name = formData.get('name') as string
+        rename({ deviceId: device._id, name })
+      }}
+    >
+      <Input name="name" defaultValue={device.name} placeholder="Enter new device name" />
+      <Button type="submit">Rename</Button>
+    </form>
   )
 }

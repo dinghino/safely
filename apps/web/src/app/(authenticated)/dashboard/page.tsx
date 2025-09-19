cb19456918dc5f99f887fdf036c4001e4d4e29e6
dinghino <dinghino@users.noreply.github.com>
2025-09-20 01:06:25 +0200

    ♻️ (web) dashboard page fix up
    a bit nicer and with functional todos

--- apps/web/src/app/(authenticated)/dashboard/page.tsx
+++ apps/web/src/app/(authenticated)/dashboard/page.tsx
@@ -1,25 +1,37 @@
 // import { Authenticated, AuthLoading, Unauthenticated } from 'convex/react'
 // import DeviceInfoComponent from './device-info'
-import { Card, CardContent, CardHeader } from '@workspace/ui/components/card'
-import { DevicesTable } from '@/features/device-manager'
+import { Card, CardContent, CardHeader, CardTitle } from '@workspace/ui/components/card'
 import { RegisterDeviceButton } from '@/features/device-tracking'
-// import { DashboardHero } from './dashboard.hero'
+import { TodoForm, TodosList } from '@/features/todos/components'
+import { DevicesTable } from '@/widgets/devices-table'
+import { DashboardHero } from './dashboard.hero'
 
 export default function Dashboard() {
   return (
-    <main className="py-4 content-grid">
-      {/* <DashboardHero /> */}
-      {/* <div><DeviceInfoComponent /></div> */}
-      <section>
-        <Card>
-          <CardHeader>Device Manager</CardHeader>
-          <CardContent>
-            <RegisterDeviceButton />
-          </CardContent>
+    <main className="max-w-full py-4 content-grid">
+      <DashboardHero />
+      <section className="grid grid-cols-1 gap-4 xl:grid-cols-7">
+        <Card className="xl:col-span-5">
+          <CardHeader>
+            <CardTitle className="font-bold text-lg">Device Manager</CardTitle>
+          </CardHeader>
           <CardContent>
             <DevicesTable />
           </CardContent>
         </Card>
+
+        <aside className="flex flex-col gap-4 xl:col-span-2">
+          <RegisterDeviceButton className="self-end" />
+          <Card className="h-full">
+            <CardContent className="flex flex-col gap-4">
+              <div className="space-y-2">
+                {/* <TodosCard /> */}
+                <TodoForm />
+                <TodosList />
+              </div>
+            </CardContent>
+          </Card>
+        </aside>
       </section>
     </main>
   )

9330bf4ee4f01b790bf7526342c16d19e5d46abc
dinghino <dinghino@users.noreply.github.com>
2025-09-20 16:46:29 +0200

    ♻️ dynamic register button on devices table
    registering devices can now be only done
    
    - from the devices table
    - if the device is not already registered

--- apps/web/src/app/(authenticated)/dashboard/page.tsx
+++ apps/web/src/app/(authenticated)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 // import { Authenticated, AuthLoading, Unauthenticated } from 'convex/react'
 // import DeviceInfoComponent from './device-info'
 import { Card, CardContent, CardHeader, CardTitle } from '@workspace/ui/components/card'
-import { RegisterDeviceButton } from '@/features/device-tracking'
 import { CreateTodoForm, TodosList } from '@/features/todos/components'
 import { DevicesTable } from '@/widgets/devices-table'
 import { DashboardHero } from './dashboard.hero'
@@ -21,7 +20,6 @@ export default function Dashboard() {
         </Card>
 
         <aside className="flex flex-col gap-4 xl:col-span-2">
-          <RegisterDeviceButton className="self-end" />
           <Card className="h-full">
             <CardContent className="flex flex-col gap-4">
               <div className="space-y-2">

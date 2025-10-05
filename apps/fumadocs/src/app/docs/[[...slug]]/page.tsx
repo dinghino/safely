378a142299605dd4ff9f9b0ae17b3d58c555b44f
dinghino <dinghino@users.noreply.github.com>
2025-10-05 12:42:10 +0200

    📝 add fumadocs components
    - type table and tabs

--- apps/fumadocs/src/app/docs/[[...slug]]/page.tsx
+++ apps/fumadocs/src/app/docs/[[...slug]]/page.tsx
@@ -10,14 +10,17 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
   const page = source.getPage(params.slug)
   if (!page) notFound()
 
-  const MDXContent = page.data.body
-
+  const { body: Mdx, toc, lastModified } = page.data
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={toc}
+      full={page.data.full}
+      lastUpdate={lastModified ? new Date(lastModified) : undefined}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
-        <MDXContent
+        <Mdx
           components={getMDXComponents({
             // this allows you to link to other pages with relative file paths
             a: createRelativeLink(source, page),

ff3bb2e829382dde0fc79c83976ab45d3b70b67b
dinghino <dinghino@users.noreply.github.com>
2025-10-09 19:26:10 +0200

    🚧 dashboard: playground with devices map

--- apps/web/src/app/(authenticated)/dashboard/page.tsx
+++ apps/web/src/app/(authenticated)/dashboard/page.tsx
@@ -1,36 +1,57 @@
 // import { Authenticated, AuthLoading, Unauthenticated } from 'convex/react'
 // import DeviceInfoComponent from './device-info'
-import { Card, CardContent, CardHeader, CardTitle } from '@workspace/ui/components/card'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@workspace/ui/components/card'
 import { CreateTodoForm, TodosList } from '@/features/todos/components'
 import { DevicesTable } from '@/widgets/devices-table'
 import { DashboardHero } from './dashboard.hero'
+import { LastKnownLocationMap } from '@/views/map-last-known-locations'
 
 export default function Dashboard() {
   return (
-    <main className="max-w-full py-4 content-grid">
+    <main className="max-w-full gap-4 py-4 content-grid">
       <DashboardHero />
-      <section className="grid grid-cols-1 gap-4 xl:grid-cols-7">
-        <Card className="xl:col-span-5">
-          <CardHeader>
-            <CardTitle className="font-bold text-lg">Device Manager</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <DevicesTable />
-          </CardContent>
-        </Card>
-
+      <div className="inline-flex gap-8 align-start">
+        <section className="grid grid-cols-1 gap-2 xl:grid-cols-7">
+          <Card className="xl:col-span-5">
+            <CardHeader>
+              <CardTitle className="font-bold text-lg">Device Manager</CardTitle>
+            </CardHeader>
+            <CardContent className="max-h-[400px] overflow-y-auto">
+              <DevicesTable />
+            </CardContent>
+          </Card>
+          <Card className="overflow-hidden pb-0 xl:col-span-2">
+            <CardHeader>
+              <CardTitle>Your devices</CardTitle>
+              <CardDescription>Glance at where your devices are</CardDescription>
+            </CardHeader>
+            <div className="h-full w-full">
+              <LastKnownLocationMap />
+            </div>
+          </Card>
+        </section>
         <aside className="flex flex-col gap-4 xl:col-span-2">
           <Card className="h-full">
+            <CardHeader>
+              <CardTitle>Your Todos</CardTitle>
+              <CardDescription>Your quick tasks and to-dos</CardDescription>
+            </CardHeader>
             <CardContent className="flex flex-col gap-4">
-              <div className="space-y-2">
+              <CreateTodoForm />
+              <div className="max-h-[400px] space-y-2 overflow-y-auto">
                 {/* <TodosCard /> */}
-                <CreateTodoForm />
                 <TodosList />
               </div>
             </CardContent>
           </Card>
         </aside>
-      </section>
+      </div>
     </main>
   )
 }

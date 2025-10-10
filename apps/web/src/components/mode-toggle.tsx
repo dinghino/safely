1a99847341818cbed7afc8c39f7b806f795023f1
dinghino <dinghino@users.noreply.github.com>
2025-10-10 11:57:27 +0200

    ✨ theme mode toggle customization
    allow button props to be passed to customize the button appearance

--- apps/web/src/components/mode-toggle.tsx
+++ apps/web/src/components/mode-toggle.tsx
@@ -10,13 +10,15 @@ import {
   DropdownMenuTrigger,
 } from '@workspace/ui/components/dropdown-menu'
 
-export function ModeToggle() {
+export function ModeToggle(
+  props: Omit<React.ComponentProps<typeof Button>, 'ref' | 'asChild' | 'children'>,
+) {
   const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
+        <Button variant="ghost" size="icon" {...props}>
           <Sun className="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>

f3cddc258f48805df4c6eb9783bc4b551f4d606b
dinghino <dinghino@users.noreply.github.com>
2025-10-10 11:57:37 +0200

    💄 dashboard playground

--- apps/web/src/app/(authenticated)/dashboard/page.tsx
+++ apps/web/src/app/(authenticated)/dashboard/page.tsx
@@ -11,33 +11,35 @@ import { CreateTodoForm, TodosList } from '@/features/todos/components'
 import { DevicesTable } from '@/widgets/devices-table'
 import { DashboardHero } from './dashboard.hero'
 import { LastKnownLocationMap } from '@/views/map-last-known-locations'
+import { cn } from '@/lib/utils'
 
 export default function Dashboard() {
   return (
-    <main className="max-w-full gap-4 py-4 content-grid">
+    <main className="max-w-full py-4 content-grid">
       <DashboardHero />
-      <div className="inline-flex gap-8 align-start">
-        <section className="grid grid-cols-1 gap-2 xl:grid-cols-7">
-          <Card className="xl:col-span-5">
-            <CardHeader>
-              <CardTitle className="font-bold text-lg">Device Manager</CardTitle>
-            </CardHeader>
-            <CardContent className="max-h-[400px] overflow-y-auto">
-              <DevicesTable />
-            </CardContent>
-          </Card>
-          <Card className="overflow-hidden pb-0 xl:col-span-2">
-            <CardHeader>
-              <CardTitle>Your devices</CardTitle>
-              <CardDescription>Glance at where your devices are</CardDescription>
-            </CardHeader>
-            <div className="h-full w-full">
-              <LastKnownLocationMap />
-            </div>
-          </Card>
-        </section>
-        <aside className="flex flex-col gap-4 xl:col-span-2">
-          <Card className="h-full">
+      {/* <div className="inline-flex"> */}
+      <section className="grid grid-cols-12 gap-4">
+        <Card className={cn('col-span-full md:col-span-full lg:col-span-6 xl:col-span-5')}>
+          <CardHeader>
+            <CardTitle className="font-bold text-lg">Device Manager</CardTitle>
+          </CardHeader>
+          <CardContent className="max-h-[400px] overflow-y-auto">
+            <DevicesTable />
+          </CardContent>
+        </Card>
+        <Card className={cn('col-span-full lg:col-span-6 xl:col-span-5', 'overflow-hidden pb-0')}>
+          <CardHeader>
+            <CardTitle>Your devices</CardTitle>
+            <CardDescription>Glance at where your devices are</CardDescription>
+          </CardHeader>
+          <div className="isolate h-full w-full">
+            <LastKnownLocationMap />
+          </div>
+        </Card>
+        <aside
+          className={cn('col-span-full lg:col-span-4 xl:col-span-2', 'flex flex-col gap-4')}
+        >
+          <Card className="max-h-fit">
             <CardHeader>
               <CardTitle>Your Todos</CardTitle>
               <CardDescription>Your quick tasks and to-dos</CardDescription>
@@ -51,7 +53,8 @@ export default function Dashboard() {
             </CardContent>
           </Card>
         </aside>
-      </div>
+      </section>
+      {/* </div> */}
     </main>
   )
 }

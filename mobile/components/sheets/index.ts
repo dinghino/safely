d553cacba439b2b7de623469b93b441ed8788d69
dinghino <dinghino@users.noreply.github.com>
2025-10-30 23:53:44 +0100

    ✨ (mobile) debug sheets for geolocation data

--- mobile/components/sheets/index.ts
+++ mobile/components/sheets/index.ts
@@ -3,12 +3,15 @@
  * used in the app with react-native-actions-sheet.
  * @see https://rnas.vercel.app/guides/sheetmanager
  */
-import { registerSheet, type SheetDefinition } from 'react-native-actions-sheet'
+import { registerSheet, RouteDefinition, type SheetDefinition } from 'react-native-actions-sheet'
 
-import { DeviceSettingsSheet } from '@/components/sheets/device-settings'
-
+import { DeviceSettingsSheet } from './device-settings'
+import { DebugLocationsDataSheet } from './debug-locations-data'
+import { DebugGeoEventsSheet } from './debug-geo-events'
 
 registerSheet('device-settings', DeviceSettingsSheet)
+registerSheet('debug-locations-data', DebugLocationsDataSheet)
+registerSheet('debug-geo-events', DebugGeoEventsSheet)
 
 // We extend some of the types here to give us great intellisense
 // across the app for all registered sheets.
@@ -16,6 +19,16 @@ declare module 'react-native-actions-sheet' {
   interface Sheets {
     'device-settings': SheetDefinition<{
       payload: DeviceSettingsSheet.Props
+    }>
+    'debug-locations-data': SheetDefinition<{
+      payload: DebugLocationsDataSheet.Props
+    }>
+    'debug-geo-events': SheetDefinition<{
+      payload: DebugGeoEventsSheet.Props,
+      routes: {
+        'events-list': RouteDefinition
+        'events-filter': RouteDefinition
+      }
     }>
   }
 }

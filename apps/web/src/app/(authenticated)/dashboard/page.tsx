8e6ddcdcace18489324106955e4c75fd0fa7e5aa
dinghino <dinghino@users.noreply.github.com>
2025-10-30 14:08:25 +0100

    ♻️ (web) dashboard widgets layout
    merge table and map widgets into one view to make them larger

--- apps/web/src/app/(authenticated)/dashboard/page.tsx
+++ apps/web/src/app/(authenticated)/dashboard/page.tsx
@@ -1,5 +1,3 @@
-// import { Authenticated, AuthLoading, Unauthenticated } from 'convex/react'
-// import DeviceInfoComponent from './device-info'
 import {
   Card,
   CardContent,
@@ -12,6 +10,7 @@ import { DevicesTable } from '@/widgets/devices-table'
 import { DashboardHero } from './dashboard.hero'
 import { LastKnownLocationMap } from '@/views/map-last-known-locations'
 import { cn } from '@/lib/utils'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@workspace/ui/components/tabs'
 
 export default function Dashboard() {
   return (
@@ -19,26 +18,37 @@ export default function Dashboard() {
       <DashboardHero />
       {/* <div className="inline-flex"> */}
       <section className="grid grid-cols-12 gap-4">
-        <Card className={cn('col-span-full md:col-span-full lg:col-span-6 xl:col-span-5')}>
-          <CardHeader>
-            <CardTitle className="font-bold text-lg">Device Manager</CardTitle>
-          </CardHeader>
-          <CardContent className="max-h-[400px] overflow-y-auto">
-            <DevicesTable />
-          </CardContent>
-        </Card>
-        <Card className={cn('col-span-full lg:col-span-6 xl:col-span-5', 'overflow-hidden pb-0')}>
-          <CardHeader>
-            <CardTitle>Your devices</CardTitle>
-            <CardDescription>Glance at where your devices are</CardDescription>
-          </CardHeader>
-          <div className="isolate h-full w-full">
-            <LastKnownLocationMap />
-          </div>
-        </Card>
-        <aside
-          className={cn('col-span-full lg:col-span-4 xl:col-span-2', 'flex flex-col gap-4')}
+        <Card
+          className={cn(
+            'col-span-full md:col-span-full lg:col-span-10',
+            'px-0 pb-0',
+            'overflow-hidden',
+          )}
         >
+          <Tabs className="h-full">
+            <CardHeader className="flex flex-row justify-between">
+              <div className="space-y-1">
+                <CardTitle className="font-bold text-lg">Your devices</CardTitle>
+                <CardDescription>Glance at where your devices are</CardDescription>
+              </div>
+
+              <TabsList>
+                <TabsTrigger value="table">Table</TabsTrigger>
+                <TabsTrigger value="map">Map</TabsTrigger>
+              </TabsList>
+            </CardHeader>
+            <CardContent className="h-full px-0 pb-0">
+              <TabsContent value="table" className="px-6">
+                <DevicesTable />
+              </TabsContent>
+              <TabsContent value="map" className="h-full px-0 pb-0">
+                <LastKnownLocationMap />
+              </TabsContent>
+            </CardContent>
+          </Tabs>
+        </Card>
+
+        <aside className={cn('col-span-full lg:col-span-4 xl:col-span-2', 'flex flex-col gap-4')}>
           <Card className="max-h-fit">
             <CardHeader>
               <CardTitle>Your Todos</CardTitle>

21a529f00f59657ff1bf509b9e1683daf6b4527f
dinghino <dinghino@users.noreply.github.com>
2025-11-01 12:15:54 +0100

    🚧 (web) add blank devices page

---
+++ apps/web/src/app/(authenticated)/devices/page.tsx
@@ -0,0 +1,8 @@
+export default function DevicesPage() {
+  return (
+    <div className="p-4">
+      <h1 className="mb-4 font-bold text-2xl">Devices</h1>
+      <p>This is the Devices page.</p>
+    </div>
+  )
+}

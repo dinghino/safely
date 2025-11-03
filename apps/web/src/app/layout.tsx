c0070dfbcf3a80e7f45b52fa0f3156f64f5a44fb
dinghino <dinghino@users.noreply.github.com>
2025-11-03 14:54:11 +0100

    ♻️ (web) general layout styling

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <DashboardSidebar />
             <SidebarInset>
               <div className="flex min-h-svh w-full flex-col">
-                <div className="sticky top-0 z-50 inline-flex min-h-(--header-height) w-full items-center border-b bg-sidebar *:w-full">
+                <div className="sticky top-0 z-50 inline-flex min-h-(--header-height) items-center border-b bg-sidebar *:w-full">
                   <Header links={links} left={<SidebarTrigger className="cursor-pointer" />} />
                 </div>
                 <div className="isolate flex flex-1 flex-col">{children}</div>

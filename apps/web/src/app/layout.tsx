44807a4b7052e9f4a3f3556e2ea9679d4bd0575f
dinghino <dinghino@users.noreply.github.com>
2025-11-30 18:16:37 +0100

    🐛 (web) tailwind fix attempt

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import '@workspace/ui/globals.css'
+// import '@workspace/ui/globals.css'
 import './styles.css'
 import '@workspace/data-filter/styles.css'
 

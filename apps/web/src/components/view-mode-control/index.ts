92491025f4f0383bed1472f14d4e5304538dd1f9
dinghino <dinghino@users.noreply.github.com>
2025-12-31 15:35:13 +0100

    ♻️ (web) view mode control w/ localstorage
    - improve UI for view controller
    - store in local storage for state persistance
    
    we'll move to some user options down the line

--- apps/web/src/components/view-mode-control/index.ts
+++ apps/web/src/components/view-mode-control/index.ts
@@ -1,1 +1,2 @@
 export * from './view-mode-context'
+export * from './view-mode-control'
--- apps/web/src/components/view-mode-control/view-mode-context.tsx
+++ apps/web/src/components/view-mode-control/view-mode-context.tsx
@@ -1,21 +1,8 @@
 'use client'
 
-import { useState } from 'react'
-import { List, Grid } from 'lucide-react'
-
 import { createContext } from '@workspace/react-utils'
-import { Button } from '@workspace/ui/components/button'
-import { ButtonGroup } from '@workspace/ui/components/button-group'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@workspace/ui/components/dropdown-menu'
+
+import { useLocalStorage } from '@/shared/hooks/use-local-storage'
 
 /**
  * A context controller for toggling view modes
@@ -27,6 +14,8 @@ export namespace ViewMode {
     setMode: (mode: Mode) => void
   }
   export type Props = {
+    defaultMode?: Mode
+    storageKey?: string
     children: React.ReactNode
   }
 }
@@ -35,39 +24,10 @@ const [Provider, useViewMode] = createContext<ViewMode.Value>()
 export { useViewMode }
 
 export const ViewModeProvider = (props: ViewMode.Props) => {
-  const { children } = props
-  const [mode, setMode] = useState<ViewMode.Mode>('list')
+  const { children, storageKey = 'view-mode', defaultMode = 'list' } = props
+  const [mode, setMode] = useLocalStorage<ViewMode.Mode>({
+    key: storageKey,
+    defaultValue: defaultMode
+  })
   return <Provider value={{ mode, setMode }}>{children}</Provider>
-}
-
-/**
- * ButtonGroup with a view mode menu to allow toggling view modes.
- * It shows current view and a dropdown button with other available views.
- */
-export function ViewModeControl() {
-  const { mode, setMode } = useViewMode()
-  return (
-    <ButtonGroup className="items-center">
-      <p className="rounded-md border px-2 py-1.5 text-xs">{mode.toUpperCase()}</p>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            {mode === 'list' ? <List /> : <Grid />}
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>View mode</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem disabled={mode === 'list'} onClick={() => setMode('list')}>
-            <List />
-            List
-          </DropdownMenuItem>
-          <DropdownMenuItem disabled={mode === 'grid'} onClick={() => setMode('grid')}>
-            <Grid />
-            Grid
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </ButtonGroup>
-  )
 }

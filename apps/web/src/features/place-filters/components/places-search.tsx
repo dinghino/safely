cbc9b64546b2ce54afe225d81d6d611000a27940
dinghino <dinghino@users.noreply.github.com>
2026-01-04 14:41:02 +0100

    ♻️ (web) places filters: improve search input

--- apps/web/src/features/place-filters/components/places-search.tsx
+++ apps/web/src/features/place-filters/components/places-search.tsx
@@ -1,13 +1,21 @@
 'use client'
 
-import { Search } from 'lucide-react'
+import { Search, XIcon } from 'lucide-react'
 import { InputGroup, InputGroupAddon, InputGroupInput } from '@workspace/ui/components/input-group'
 import { usePlaceFilters } from '../context/place-filters-provider'
+import { Button } from '@workspace/ui/components/button'
+import { cn } from '@/lib/utils'
+
+export namespace PlacesNameSearchFilter {
+  export type Props = React.ComponentProps<typeof InputGroupInput> & {
+    input?: React.ComponentProps<typeof InputGroupInput>
+  }
+}
 
 /**
  * Search filter for places by name.
  */
-export function PlacesNameSearchFilter() {
+export function PlacesNameSearchFilter({ input, ...props }: PlacesNameSearchFilter.Props) {
   const { searchQuery, setSearchQuery } = usePlaceFilters()
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,19 +23,26 @@ export function PlacesNameSearchFilter() {
   }
 
   return (
-    <InputGroup>
+    <InputGroup {...props}>
       <InputGroupInput
         id="search-filter"
         placeholder="Search places..."
+        {...input}
+        className={cn('w-full', input?.className)}
         value={searchQuery}
         onChange={handleSearchChange}
-        className="w-full"
       />
       <InputGroupAddon>
         <Search />
       </InputGroupAddon>
       {/* we can show the results count here if we hook up to the list also */}
-      <InputGroupAddon align="inline-end">{/*12 results*/}</InputGroupAddon>
+      <InputGroupAddon align="inline-end">
+        {searchQuery && (
+          <Button variant="ghost" size="icon" onClick={() => setSearchQuery('')} className="size-6">
+            <XIcon />
+          </Button>
+        )}
+      </InputGroupAddon>
     </InputGroup>
   )
 }
